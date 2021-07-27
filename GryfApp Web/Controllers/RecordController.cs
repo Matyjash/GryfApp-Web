d81957423d476fa7ff05a792b056f529ce71dc7b
Matyjash <lengom4w@gmail.com>
2021-07-27 22:09:20 +0200

    Record displaying added
    -displaying records in Record/Index view
    -connectring with Firebase database
    -records filtration (date)

--- GryfApp Web/Controllers/RecordController.cs
+++ GryfApp Web/Controllers/RecordController.cs
@@ -6,13 +6,55 @@ using System.Threading.Tasks;
 using Firebase.Database;
 using Firebase.Database.Query;
 using System.Web;
+using GryfApp_Web.Models;
 
 namespace GryfApp_Web.Controllers
 {
     public class RecordController : Controller
     {
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
+
+            //Firebase client
+            var firebaseClient = new FirebaseClient("https://ehhapp-5467e.firebaseio.com/");
+            //List for received records from database
+            var recordsList = new List<Record>();
+
+
+            /*
+            var result = await firebaseClient
+                .Child("data")
+                .Child("records")
+                .PostAsync(exampleRecord);
+            */
+
+
+            //Retrieving entries data from Firebase database
+            var dbRecord = await firebaseClient
+                .Child("data")
+                .Child("records")
+                .OnceAsync<Record>();
+
+            var yesterday = DateTime.Now;
+            yesterday.AddDays(-1);
+            foreach (var record in dbRecord)
+            {
+                if (Convert.ToDateTime(record.Object.userDate).ToLocalTime().CompareTo(yesterday) > (-1))
+                {
+                    Record newRecord = new Record(record.Object.recordId, record.Object.userDate, record.Object.userId, record.Object.userName, record.Object.userParts);
+                    recordsList.Add(newRecord);
+                }
+            }
+
+            // TODO: Sorting list of records
+
+            //Pasing data to the view
+            ViewBag.Records = recordsList;
+
+           
+            System.Diagnostics.Debug.WriteLine("Number of records:" + recordsList.Count);
+
             return View();
         }
     }
--- GryfApp Web/Models/Record.cs
+++ GryfApp Web/Models/Record.cs
@@ -7,5 +7,23 @@ namespace GryfApp_Web.Models
 {
     public class Record
     {
+        public Record() { }
+        public Record(String recordId, String userDate, String userId, String userName, String userParts)
+        {
+            this.recordId = recordId;
+            this.userDate = userDate;
+            this.userId = userId;
+            this.userName = userName;
+            this.userParts = userParts;
+        }
+        public String recordId { get; set; }
+
+        public String userDate { get; set; }
+
+        public String userId { get; set; }
+
+        public String userName { get; set; }
+
+        public String userParts { get; set; }
     }
 }
