96bca6113dc0694fd0408c4759f31eb490a21c44
Matyjash <lengom4w@gmail.com>
2021-07-27 20:49:32 +0200

    First steps

---
+++ GryfApp Web/Controllers/RecordController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GryfApp_Web.Controllers
+{
+    public class RecordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GryfApp Web/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GryfApp_Web.Models
+{
+    public class Record
+    {
+    }
+}
