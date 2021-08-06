c631700adb5a74a40b1b02fdc789bac6abcc82c9
p-rja <p-rja@astec.net>
2021-08-06 11:27:40 +0200

    Added artificial login

--- GryfApp Web/Controllers/LoginController.cs
+++ GryfApp Web/Controllers/LoginController.cs
@@ -8,44 +8,15 @@ namespace GryfApp_Web.Controllers
 {
     public class LoginController : Controller
     {
-        private static string ApiKey = "AIzaSyDG_Jhltdmbi6qAmbFHusUUNO0dGx3dPDM";
-        private static string Bucket = "https://ehhapp-5467e.firebaseio.com/";
-        // GET: Account
-
         public ActionResult Index()
 		{
+
             return View();
 		}
 
-        [HttpPost]
-        public ActionResult Login(GryfApp_Web.Models.User user)
-		{
-            string email = user.email;
-            string pss = user.pss;
-
-            var authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyDG_Jhltdmbi6qAmbFHusUUNO0dGx3dPDM"));
-
-            var auth = authProvider.SignInWithEmailAndPasswordAsync(email, pss);
-            try
-            {
-                if (auth.Result.User.IsEmailVerified)
-                {
-                    return View("Login", user);
-                }
-            } catch (FirebaseAuthException e)
-            {
-                return View("Index");
-            } catch (HttpRequestException e)
-			{
-                return View("Index");
-            }catch(System.AggregateException e)
-			{
-                return View("Index");
-			}
-
+        public ActionResult Login()
+		{   
             return View();
-			
-
         }
 
 
---
+++ GryfApp Web/wwwroot/js/user.js
@@ -0,0 +1,54 @@
+﻿var firebaseConfig = {
+    apiKey: "AIzaSyDG_Jhltdmbi6qAmbFHusUUNO0dGx3dPDM",
+    authDomain: "ehhapp-5467e.firebaseapp.com",
+    databaseURL: "https://ehhapp-5467e.firebaseio.com",
+    projectId: "ehhapp-5467e",
+    storageBucket: "ehhapp-5467e.appspot.com",
+    messagingSenderId: "830836252123",
+    appId: "1:830836252123:web:a861e8a8594dc9a7355bfe",
+    measurementId: "G-QB4STXJBQL"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const bttnSignIn = document.getElementById('signIn');
+const bttnSignOut = document.getElementById('signOut');
+const txtEmail = document.getElementById('email');
+const txtPassword = document.getElementById('password');
+
+setTimeout(() => {
+    bttnSignIn.addEventListener('click', e => {
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(txtEmail.value, txtPassword.value);
+        promise.catch(e => console.log(e.message));
+    });
+
+
+}, 200);
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log("zalogowany");
+        document.getElementById("login-form").style.display = 'none';
+        document.getElementById("schedule_layout").style.display = "";
+    } else {
+        console.log("niezalogowany");
+        document.getElementById("schedule_layout").style.display = "none";
+        if (window.location.href == "https://localhost:44389/Record" || window.location.href == "https://localhost:44389/Record/ShowRecord" || window.location.href == "https://localhost:44389/Record/ErrorInput"
+            || window.location.href == "https://localhost:44389/Record/Create") {
+            window.location.replace("https://localhost:44389/Login")
+        }
+        }
+    
+
+});
+
+setTimeout(() => {
+    bttnSignOut.addEventListener('click', e => {
+        const auth = firebase.auth();
+        auth.signOut();
+        document.getElementById("login-form").style.display = '';
+    });
+
+
+}, 200);
